e2365bf67b0678dd4b5d41090208c89f298aaa73
Scott  Keplinger <skeplin17@gmail.com>
2020-05-15 15:49:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,67 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    @@all << self.new
+    @@all.last
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+end
+
+  def self.find_by_name(name)
+     self.all.find {|song| song.name == name}
+   end
+
+   def self.find_or_create_by_name(name)
+     if self.find_by_name(name)
+        self.find_by_name(name)
+     else
+        self.create_by_name(name)
+      end
+   end
+
+   def self.alphabetical
+     self.all.sort_by {|song| song.name }
+   end
+
+   def self.new_from_filename(file_name)
+     song = self.new
+     file_split = []
+     file_split = file_name.split(/[-.]/)
+     artist_name = file_split[0].strip
+     song.name = file_split[1].strip
+     song.artist_name = artist_name
+     song
+   end
+
+   def self.create_from_filename(file_name)
+     song = self.new
+     file_split = []
+     file_split = file_name.split(/[-.]/)
+     artist_name = file_split[0].strip
+     song.name = file_split[1].strip
+     song.artist_name = artist_name
+     self.all << song
+     song
+   end
+
+   def self.destroy_all
+     self.all.clear()
+   end
 
   def self.all
     @@all
